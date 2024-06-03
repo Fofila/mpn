3cd627f4f6ccd26d4c7667a1f037e7d55c2fb82b
Fofila <zorzi.filippo@gmail.com>
2024-06-03 08:33:23 +0200

    create structure

---
+++ mpn.js
@@ -0,0 +1,21 @@
+/*
+ *
+ * Params:
+ *  - type: string
+ *  - sender: string
+ *  - description
+ *  - time
+ *  - kwargs:
+ *     - media
+ *     - link
+ *     - cta
+ *     - cta
+ *     - cta
+ *     - cta
+ *     - attachment
+ *     - close button
+ *     - timer
+ *     - title
+ *     - icon
+ *     - custom style
+ */
